3ba5f2074c9ce027ccca74679b7b010c7f2aa19b
piyush <piyushyadav832988@gmail.com>
2025-02-04 21:16:16 +0530

    SOlved the cors policy error and handled the api calls

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
 
     # Your apps
     "marketplace",  # Register the marketplace app here
+     "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +54,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    # Other middleware
 ]
+
+
+CORS_ALLOWED_ORIGINS = [ 
+    "http://localhost:5173",  # React frontend URL
+]
+
 
 ROOT_URLCONF = 'backend.urls'
 
