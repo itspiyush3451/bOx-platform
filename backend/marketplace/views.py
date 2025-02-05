afd9326e60273e1f731bab318df4934be0897119
piyush <piyushyadav832988@gmail.com>
2025-02-05 21:59:07 +0530

    started with the notes section in the backend

--- backend/marketplace/views.py
+++ backend/marketplace/views.py
@@ -3,6 +3,9 @@ from rest_framework import generics
 from .models import Book, Note
 from .serializers import BookSerializer, NoteSerializer
 from django.http import JsonResponse
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status, generics
 
 def home(request):
     return JsonResponse({"message": "Welcome to the bOx API!"})
@@ -16,3 +19,11 @@ class BookListCreateView(generics.ListCreateAPIView):
 class NoteListCreateView(generics.ListCreateAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    parser_classes = (MultiPartParser , FormParser)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
